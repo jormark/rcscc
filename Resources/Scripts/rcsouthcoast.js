a12b49be1d457033beda99576dd86512ed88322f
Jormark <jordancmarkham@outlook.com>
2021-04-01 01:17:27 +0100

    Added scoring

--- Resources/Scripts/rcsouthcoast.js
+++ Resources/Scripts/rcsouthcoast.js
@@ -1,3 +1,4 @@
+var score = 0;
 var startScreen = document.querySelector('.startScreen');
 var funSplash = document.querySelector('.funSplash');
 var compSplash = document.querySelector('.compSplash');
@@ -6,3 +7,11 @@ var compButton = document.querySelector('.comp');
 var mainScreen = document.querySelector('.mainScreen');
 var funScreen = document.querySelector('.funScreen');
 var compScreen = document.querySelector('.compScreen');
+var scoreCard = document.querySelector('.scoreCard');
+
+function changeScore(){
+    var displayScore = document.querySelectorAll('.score');
+    for (var i=0; i < displayScore.length; i++) {
+        displayScore[i].innerHTML = score;
+    }
+};
