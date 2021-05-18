0896abe4fa1600c33b05e2dc17d2ad309ed5f10e
Jormark <jordancmarkham@outlook.com>
2021-05-18 03:07:29 +0100

    Most recent update

--- Resources/Scripts/rcsouthcoast.js
+++ Resources/Scripts/rcsouthcoast.js
@@ -7,7 +7,8 @@ var compButton = document.querySelector('.comp');
 var mainScreen = document.querySelector('.mainScreen');
 var funScreen = document.querySelector('.funScreen');
 var compScreen = document.querySelector('.compScreen');
-var scoreCard = document.querySelector('.scoreCard');
+var scoreCard = document.querySelectorAll('.scoreCard');
+var settings = document.querySelectorAll('.settings');
 var rulesetInUse;
 
 var pointTypes = {
@@ -56,72 +57,97 @@ var sorrcaRuleset = {
     handWinch: 10
 }
 
+window.onload = function () {
+    generate();
+}
+
+/**Currently, the program allows you to switch between SORRCA Lite and SORRCA. However, the 
+ * old selection of scorecard buttons cannot be removed accurately without adjustments being made to the program.
+ * 
+ * These adjustments were unable to be added in time, but would have involved the deletion of old scorecards and subsequent replacement with the new/relevant scorecards
+ * for the correct ruleset, allowing for the UI to update alongside the program.
+ * 
+ * The program behind the interface works as expected and allows the user to switch between rulesets - however, the interface is suffering with some techincal difficulties
+ * in replacing the scorecards.
+ */
+
 function generate() {
+    if (liteRuleset.inUse == true) {
+        rulesetInUse = liteRuleset;
+    }
+    else if (sorrcaRuleset.inUse == true) {
+        rulesetInUse = sorrcaRuleset;
+    }
     var rowArea = document.querySelectorAll('.scorePoints');
-    for (var i = 0; i < pointTypes.length; i++) {
-        var pointRow = document.createElement("div"),
-            pointType = document.createElement("h"),
-            changePoints = document.createElement("div"),
-            deductPoint = document.createElement("div"),
-            addPoint = document.createElement("div"),
-            minus = document.createElement("h"),
-            plus = document.createElement("h");
-        pointRow.setAttribute("class", "pointRow");
-        pointType.setAttribute("class", "pointType");
-        changePoints.setAttribute("class", "changePoints");
-        deductPoint.setAttribute("class", "deductPoint");
-        addPoint.setAttribute("class", "addPoint");
-        minus.innerHTML = "-";
-        plus.innerHTML = "+";
-        switch (pointTypes[i]) {
-            case "progress":
-                pointType.innerHTML = "Progress";
-                deductPoint.setAttribute("onclick", "deductPoint('progress')");
-                addPoint.setAttribute("onclick", "addPoint('progress')");
-                break;
-            case "reverse":
-                pointType.innerHTML = "Reverse";
-                deductPoint.setAttribute("onclick", "deductPoint('reverse')");
-                addPoint.setAttribute("onclick", "addPoint('reverse')");
-                break;
-            case "hitGate":
-                pointType.innerHTML = "Hit gate marker";
-                deductPoint.setAttribute("onclick", "deductPoint('hitGate')");
-                addPoint.setAttribute("onclick", "addPoint('hitGate')");
-                break;
-            case "rollover":
-                pointType.innerHTML = "Rollover";
-                deductPoint.setAttribute("onclick", "deductPoint('rollover')");
-                addPoint.setAttribute("onclick", "addPoint('rollover')");
-                break;
-            case "boundary":
-                pointType.innerHTML = "Boundary";
-                deductPoint.setAttribute("onclick", "deductPoint('boundary')");
-                addPoint.setAttribute("onclick", "addPoint('boundary')");
-                break;
-            case "vehicleTouch":
-                pointType.innerHTML = "Vehicle touch (reposition)";
-                deductPoint.setAttribute("onclick", "deductPoint('vehicleTouch')");
-                addPoint.setAttribute("onclick", "addPoint('vehicleTouch')");
-                break;
-            case "winch":
-                pointType.innerHTML = "Use of winch";
-                deductPoint.setAttribute("onclick", "deductPoint('winch')");
-                addPoint.setAttribute("onclick", "addPoint('winch')");
-                break;
-            case "handWinch":
-                pointType.innerHTML = "Hand winch";
-                deductPoint.setAttribute("onclick", "deductPoint('handWinch')");
-                addPoint.setAttribute("onclick", "addPoint('handWinch')");
-                break;
+    for (var n = 0; n < rowArea.length; n++) {
+        for (var i in pointTypes) {
+            var pointRow = document.createElement("div"),
+                pointType = document.createElement("h"),
+                changePoints = document.createElement("div"),
+                deductPoint = document.createElement("div"),
+                addPoint = document.createElement("div"),
+                minus = document.createElement("h"),
+                plus = document.createElement("h");
+            pointRow.setAttribute("class", "pointRow");
+            pointType.setAttribute("class", "pointType");
+            changePoints.setAttribute("class", "changePoints");
+            deductPoint.setAttribute("class", "deductPoint");
+            addPoint.setAttribute("class", "addPoint");
+            minus.innerHTML = "-";
+            plus.innerHTML = "+";
+            switch (pointTypes[i]) {
+                case "progress":
+                    pointType.innerHTML = "Progress (-" + rulesetInUse.progress + ")";
+                    deductPoint.setAttribute("onclick", "deductPoint('progress')");
+                    addPoint.setAttribute("onclick", "addPoint('progress')");
+                    break;
+                case "reverse":
+                    pointType.innerHTML = "Reverse (+" + rulesetInUse.reverse + ")";
+                    deductPoint.setAttribute("onclick", "deductPoint('reverse')");
+                    addPoint.setAttribute("onclick", "addPoint('reverse')");
+                    break;
+                case "hitGate":
+                    pointType.innerHTML = "Hit gate marker (+" + rulesetInUse.hitGate + ")";
+                    deductPoint.setAttribute("onclick", "deductPoint('hitGate')");
+                    addPoint.setAttribute("onclick", "addPoint('hitGate')");
+                    break;
+                case "rollover":
+                    pointType.innerHTML = "Rollover (+" + rulesetInUse.rollover + ")";
+                    deductPoint.setAttribute("onclick", "deductPoint('rollover')");
+                    addPoint.setAttribute("onclick", "addPoint('rollover')");
+                    break;
+                case "boundary":
+                    pointType.innerHTML = "Boundary (+" + rulesetInUse.boundary + ")";
+                    deductPoint.setAttribute("onclick", "deductPoint('boundary')");
+                    addPoint.setAttribute("onclick", "addPoint('boundary')");
+                    break;
+                case "vehicleTouch":
+                    pointType.innerHTML = "Vehicle touch (reposition) (+" + rulesetInUse.vehicleTouch + ")";
+                    deductPoint.setAttribute("onclick", "deductPoint('vehicleTouch')");
+                    addPoint.setAttribute("onclick", "addPoint('vehicleTouch')");
+                    break;
+                case "winch":
+                    pointType.innerHTML = "Use of winch (+" + rulesetInUse.winch + ")";
+                    deductPoint.setAttribute("onclick", "deductPoint('winch')");
+                    addPoint.setAttribute("onclick", "addPoint('winch')");
+                    break;
+                case "handWinch":
+                    pointType.innerHTML = "Hand winch (+" + rulesetInUse.handWinch + ")";
+                    deductPoint.setAttribute("onclick", "deductPoint('handWinch')");
+                    addPoint.setAttribute("onclick", "addPoint('handWinch')");
+                    break;
+            }
+            deductPoint.appendChild(minus);
+            addPoint.appendChild(plus);
+            changePoints.appendChild(deductPoint);
+            changePoints.appendChild(addPoint);
+            pointRow.appendChild(pointType);
+            pointRow.appendChild(changePoints);
+            rowArea[n].appendChild(pointRow);
         }
-        deductPoint.appendChild(minus);
-        addPoint.appendChild(plus);
-        changePoints.appendChild(deductPoint);
-        changePoints.appendChild(addPoint);
-        pointRow.appendChild(pointType);
-        pointRow.appendChild(changePoints);
-        rowArea.appendChild(pointRow);
+        var fillerArea = document.createElement("div");
+        fillerArea.setAttribute("class", "filler");
+        rowArea[n].appendChild(fillerArea);
     }
 }
 
@@ -171,12 +197,6 @@ function addPoint(addBy) {
 }
 
 function deductPoint(addBy) {
-    if (liteRuleset.inUse == true) {
-        rulesetInUse = liteRuleset;
-    }
-    else if (sorrcaRuleset.inUse == true) {
-        rulesetInUse = sorrcaRuleset;
-    }
     switch (addBy) {
         case "progress":
             score += rulesetInUse.progress;
